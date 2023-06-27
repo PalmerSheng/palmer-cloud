42b97154d0075198edc5a7a0be1644839c28cd32
PalmerSheng <2359245812@qq.com>
2023-06-28 00:45:43 +0800

    1

--- palmer-mybatis-demo/src/main/java/com/palmer/biz/mybatisdemo/MyTest.java
+++ palmer-mybatis-demo/src/main/java/com/palmer/biz/mybatisdemo/MyTest.java
@@ -1,5 +1,7 @@
 package com.palmer.biz.mybatisdemo;
 
+import java.time.ZonedDateTime;
+
 /**
  * @author palmer
  * @date 2023-06-21
@@ -7,7 +9,11 @@ package com.palmer.biz.mybatisdemo;
 public class MyTest {
     private static int i = 0;
 
-    public  static  synchronized  void increment() {
+    public static synchronized void increment() {
         i++;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ZonedDateTime.now());
     }
 }
--- palmer-mybatis-demo/src/main/java/com/palmer/biz/mybatisdemo/TestController.java
+++ palmer-mybatis-demo/src/main/java/com/palmer/biz/mybatisdemo/TestController.java
@@ -1,8 +1,8 @@
 package com.palmer.biz.mybatisdemo;
 
-import com.palmer.framework.common.exception.enums.GlobalErrorCodeConstants;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,10 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2023-06-19
  */
 @RestController
-@RequestMapping("test")
+@Api(tags = {"a", "b"})
 public class TestController {
     @GetMapping("test1")
-    public String test() {
-        throw new RuntimeException(GlobalErrorCodeConstants.UNKNOWN.getMsg());
+    @ApiOperation(value = "test1")
+    public String test1() {
+        return "test1";
+    }
+
+    @GetMapping("test2")
+    @ApiOperation(value = "test2")
+    public String test2() {
+        return "test2";
+    }
+
+
+    @GetMapping("test3")
+    @ApiOperation(value = "test3")
+    public String test3() {
+        return "test3";
     }
 }
